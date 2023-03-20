da0f4513a0c1b1c4a347f3708f128787be7583d5
Osama-AbuElnour <usama.abuelnour@gmail.com>
2023-03-20 22:24:34 +0200

    first commit

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,36 +1,47 @@
-import classes from './Modal.css';
-import Backdrop from '../Backdrop/Backdrop';
-import Button from '../Button/Button';
-import Spinner from '../Spinner/spinner';
+import classes from "./Modal.css";
+import Backdrop from "../Backdrop/Backdrop";
+import Button from "../Button/Button";
+import Spinner from "../Spinner/spinner";
 
-export default (props)=>{
-    let content = <><h2>Please Wait</h2><Spinner /></>;
+export default (props) => {
+	let content = (
+		<>
+			<h2>Please Wait</h2>
+			<Spinner />
+		</>
+	);
 
-    if(props.purchase && !props.loading)
-        content= 
-             <>
-                 <h2>Your Order</h2>
-                <p>A delicious burger with the following ingredients :</p>
-                <ul style={{display: 'inline-block'}}>
-                    {Object.keys(props.ing).map(key=><li key={key}>{key}: {props.ing[key]}</li>)}
-                </ul>
-                <h4 style={{display: 'inline-block', float: 'right', margin: '60px 160px 0 0'}}>Total:&nbsp;&nbsp;{props.total} $</h4>
-                <p>Continue to Checkout?</p>
-                <Button type='danger' close={props.close}>Cancel</Button>
-                <Button type='success' clicked={props.continue}>Continue</Button>
-            </>;
-                
+	if (props.purchase && !props.loading)
+		content = (
+			<>
+				<h2>Your Order</h2>
+				<p>A delicious burger with the following ingredients :</p>
+				<ul style={{ display: "inline-block" }}>
+					{Object.keys(props.ing).map((key) => (
+						<li key={key}>
+							{key}: {props.ing[key]}
+						</li>
+					))}
+				</ul>
+				<h4 className={classes.total}>Total:&nbsp;&nbsp;{props.total} $</h4>
+				<p>Continue to Checkout?</p>
+				<div className={classes.buttonGroup}>
+					<Button type="danger" close={props.close}>
+						Cancel
+					</Button>
+					<Button type="success" clicked={props.continue}>
+						Continue
+					</Button>
+				</div>
+			</>
+		);
 
-    if(props.err.state) content = <h2>{props.err.message}</h2>;
-    
-    return(
-        props.purchase ?
-        <>
-            <Backdrop close={props.close} purchase={props.purchase}/>
-            <div className={classes.modal}>
-                {content}
-            </div>;
-        </>
-        :null
-    )
+	if (props.err.state) content = <h2>{props.err.message}</h2>;
+
+	return props.purchase ? (
+		<>
+			<Backdrop close={props.close} purchase={props.purchase} />
+			<div className={classes.modal}>{content}</div>;
+		</>
+	) : null;
 };
